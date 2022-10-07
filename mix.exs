55f7cf18b43bb82c5db7e88473bd6878687d4f1f
Vitor Machado <vitormds22@gmail.com>
2022-10-06 22:45:58 -0300

    chore: add dependencies for credo and ex machina in mix and configs

--- mix.exs
+++ mix.exs
@@ -41,7 +41,9 @@ defmodule GoBillManager.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:ex_machina, "~> 2.7.0", only: :test}
     ]
   end
 
