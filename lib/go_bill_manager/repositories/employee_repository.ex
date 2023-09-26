5f9a8747d57f5f7cbc7098d6ca7ba3b8555a7dba
Vitor Machado <vitormds22@gmail.com>
2023-09-25 21:46:15 -0300

    feat: create employee repositories

---
+++ lib/go_bill_manager/repositories/employee_repository.ex
@@ -0,0 +1,48 @@
+defmodule GoBillManager.Repositories.EmployeeRepository do
+  @moduledoc """
+  Repository responsible to get values about Employees
+  """
+
+  import Ecto.Query
+
+  alias GoBillManager.Models.Employee
+  alias GoBillManager.Repo
+
+  @spec find(employee_id :: Ecto.UUID.t()) :: {:ok, Employee.t()} | {:error, :employee_not_found}
+  def find(employee_id) do
+    Employee
+    |> Repo.get(employee_id, telemetry_options: [name: :employee_repository_find])
+    |> case do
+      nil ->
+        {:error, :employee_not_found}
+
+      employee ->
+        {:ok, employee}
+    end
+  end
+
+  @spec find!(employee_id :: Ecto.UUID.t()) :: Employee.t() | no_return()
+  def find!(employee_id) do
+    employee_id
+    |> find()
+    |> case do
+      {:error, :employee_not_found} ->
+        raise Ecto.NoResultsError, queryable: Employee
+
+      {:ok, employee} ->
+        employee
+    end
+  end
+
+  @spec list_employees :: {:ok, list(Employee.t())} | {:error, :employees_not_found}
+  def list_employees do
+    Employee
+    |> from(as: :employee)
+    |> order_by([e], asc: e.inserted_at)
+    |> Repo.all(telemetry_options: [name: :employee_repository_list_employees])
+    |> case do
+      [] -> {:error, :employees_not_found}
+      employees -> {:ok, employees}
+    end
+  end
+end
