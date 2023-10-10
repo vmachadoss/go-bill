6d47a5bc29005bf451f661e907ba9527a488d75d
Vitor Machado <vitormds22@gmail.com>
2023-10-09 21:35:11 -0300

    test: improve tests for employee view

--- test/go_bill_manager_web/views/employee_view_test.exs
+++ test/go_bill_manager_web/views/employee_view_test.exs
@@ -1,4 +1,5 @@
 defmodule GoBillManagerWeb.EmployeeViewTest do
+  alias GoBillManager.Repositories.EmployeeRepository
   use GoBillManager.DataCase, async: true
 
   alias GoBillManagerWeb.EmployeeView
@@ -12,6 +13,43 @@ defmodule GoBillManagerWeb.EmployeeViewTest do
       assert employee.id == rendered_response.employee_id
       assert employee.name == rendered_response.name
       assert employee.role == rendered_response.role
+    end
+  end
+
+  describe "render/2 index.json" do
+    test "with success response" do
+      employee = insert(:employee)
+      
+      assert employees = EmployeeRepository.list_employees()
+
+      rendered_response = EmployeeView.render("index.json", %{employees: employees})
+
+      assert length(rendered_response) == 1
+
+      assert resp_employee = Enum.at(rendered_response, 0)
+      assert employee.id == resp_employee.id
+      assert employee.name == resp_employee.name
+      assert employee.role == resp_employee.role 
+    end
+
+    test "without employees" do
+      assert employees = EmployeeRepository.list_employees()
+
+      rendered_response = EmployeeView.render("index.json", %{employees: employees})
+
+      assert [] == rendered_response
+    end
+  end
+
+  describe "render/2 simplified_employee.json" do
+    test "with success response" do
+      employee = insert(:employee)
+
+      rendered_response = EmployeeView.render("simplified_employee.json", %{employee: employee})
+
+      assert employee.id == rendered_response.id
+      assert employee.name == rendered_response.name
+      assert employee.role == rendered_response.role 
     end
   end
 end
