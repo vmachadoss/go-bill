d503a4a0eae50bb016cd61609960b4ade580524d
Vitor Machado <vitormds22@gmail.com>
2023-10-30 22:06:39 -0300

    chore: create bill repository to find one bill

---
+++ lib/go_bill_manager/repositories/bill_repository.ex
@@ -0,0 +1,33 @@
+defmodule GoBillManager.Repositories.BillRepository do
+  @moduledoc """
+  Repository responsible to get values about Bills
+  """
+  alias GoBillManager.Models.Bill
+  alias GoBillManager.Repo
+
+  @spec find(bill_id :: Ecto.UUID.t()) :: {:ok, Bill.t()} | {:error, :bill_not_found}
+  def find(bill_id) do
+    Bill
+    |> Repo.get(bill_id, telemetry_options: [name: :bill_repository_find])
+    |> case do
+      nil ->
+        {:error, :bill_not_found}
+
+      bill ->
+        {:ok, bill}
+    end
+  end
+
+  @spec find!(bill_id :: Ecto.UUID.t()) :: Bill.t() | no_return()
+  def find!(bill_id) do
+    bill_id
+    |> find()
+    |> case do
+      {:error, :bill_not_found} ->
+        raise Ecto.NoResultsError, queryable: Bill
+
+      {:ok, bill} ->
+        bill
+    end
+  end
+end
