caf0f1fa38ac22fd2da997db03b3e7d725fa7808
Vitor Machado <vitormds22@gmail.com>
2023-10-30 22:11:50 -0300

    chore: undo bumpp

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule GoBillManager.MixProject do
     [
       app: :go_bill_manager,
       version: "0.1.0",
-      elixir: "~> 1.15.6",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
