1edb39817fbec5679cd7db18685d9608ab59ec63
Vitor Machado <vitormds22@gmail.com>
2023-12-11 21:29:25 -0300

    feat: add customer_table_controller

--- lib/go_bill_manager_web/controllers/customer_table_controller.ex
+++ lib/go_bill_manager_web/controllers/customer_table_controller.ex
@@ -5,6 +5,7 @@ defmodule GoBillManagerWeb.CustomerTableController do
   use GoBillManagerWeb, :controller
 
   alias GoBillManager.Commands.CustomerTableCreate
+  alias GoBillManager.Repositories.CustomerTableRepository
 
   @spec create(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def create(conn, params) do
@@ -19,9 +20,30 @@ defmodule GoBillManagerWeb.CustomerTableController do
     end
   end
 
+  @spec show(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  def show(conn, %{"id" => id}) do
+    with {:ok, customer_table_id} <- validate_uuid(id),
+         {:ok, customer_table} <- CustomerTableRepository.find(customer_table_id) do
+      render(conn, "simplified_customer_table.json", %{customer_table: customer_table})
+    else
+      {:error, :invalid_uuid} ->
+        parse_response_to_json(conn, 400, %{type: "error:invalid_customer_table_id"})
+
+      {:error, :customer_table_not_found} ->
+        parse_response_to_json(conn, 404, %{type: "error:customer_table_not_found"})
+    end
+  end
+
   defp parse_response_to_json(conn, status, value) do
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(status, Jason.encode!(value))
+  end
+
+  defp validate_uuid(customer_table_id) do
+    case Ecto.UUID.cast(customer_table_id) do
+      :error -> {:error, :invalid_uuid}
+      _ -> {:ok, customer_table_id}
+    end
   end
 end
