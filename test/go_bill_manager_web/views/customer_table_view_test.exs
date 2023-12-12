84c1d364c8f16125af2cbeccf4a3fc4fad554a10
Vitor Machado <vitormds22@gmail.com>
2023-12-11 22:14:57 -0300

    test: improve tests

--- test/go_bill_manager_web/views/customer_table_view_test.exs
+++ test/go_bill_manager_web/views/customer_table_view_test.exs
@@ -1,6 +1,7 @@
 defmodule GoBillManagerWeb.CustomerTableViewTest do
   use GoBillManager.DataCase, async: true
 
+  alias GoBillManager.Repositories.CustomerTableRepository
   alias GoBillManagerWeb.CustomerTableView
 
   describe "render/2 create.json" do
@@ -13,6 +14,33 @@ defmodule GoBillManagerWeb.CustomerTableViewTest do
       assert customer_table.id == rendered_response.customer_table_id
       assert customer_table.label == rendered_response.label
       assert customer_table.state == rendered_response.state
+    end
+  end
+
+  describe "render/2 index.json" do
+    test "with success response" do
+      customer_table = insert(:customer_table)
+
+      assert customer_tables = CustomerTableRepository.list_customer_tables()
+
+      rendered_response =
+        CustomerTableView.render("index.json", %{customer_tables: customer_tables})
+
+      assert length(rendered_response) == 1
+
+      assert resp_customer_table = Enum.at(rendered_response, 0)
+      assert customer_table.id == resp_customer_table.id
+      assert customer_table.label == resp_customer_table.label
+      assert customer_table.state == resp_customer_table.state
+    end
+
+    test "without customer_tables" do
+      assert customer_tables = CustomerTableRepository.list_customer_tables()
+
+      rendered_response =
+        CustomerTableView.render("index.json", %{customer_tables: customer_tables})
+
+      assert [] == rendered_response
     end
   end
 
