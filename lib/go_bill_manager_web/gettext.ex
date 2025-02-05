bc6a9d89280a370fc32bc79dfc05c094be6b1932
vmachadoss <vitormds22@gmail.com>
2025-02-05 08:25:29 -0300

    feat: wip

--- lib/go_bill_manager_web/gettext.ex
+++ lib/go_bill_manager_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule GoBillManagerWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :go_bill_manager
+  use Gettext.Backend, otp_app: :go_bill_manager
 end
